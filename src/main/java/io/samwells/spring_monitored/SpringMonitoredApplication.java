217798bceb6b3d979f3e60f6ebecb8f10c5a4eb7
Sam Wells <samwells95@gmail.com>
2025-06-27 16:39:51 -0700

    initializr

---
+++ src/main/java/io/samwells/spring_monitored/SpringMonitoredApplication.java
@@ -0,0 +1,13 @@
+package io.samwells.spring_monitored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonitoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMonitoredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/samwells/spring_monitored/SpringMonitoredApplicationTests.java
@@ -0,0 +1,13 @@
+package io.samwells.spring_monitored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonitoredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
