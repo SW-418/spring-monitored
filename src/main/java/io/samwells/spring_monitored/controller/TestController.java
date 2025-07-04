6a009250c3c8aa8446b5e402dbad9baf2dd924b7
Sam Wells <samwells95@gmail.com>
2025-07-04 11:07:42 -0700

    Add logging test to controller

--- src/main/java/io/samwells/spring_monitored/controller/TestController.java
+++ src/main/java/io/samwells/spring_monitored/controller/TestController.java
@@ -1,5 +1,7 @@
 package io.samwells.spring_monitored.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/test")
 public class TestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping
     public String test(){
+        LOGGER.info("GET /api/v1/test");
         return "test";
     }
 }
